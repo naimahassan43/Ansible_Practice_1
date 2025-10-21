39ebedd70710bde8fe43678e5c129bbbcea5658b
naimahassan43 <naima.hassan434@gmail.com>
2025-10-21 17:06:49 +0100

    Custom Module Exception Handling

--- 21_custom_module/library/facts_to_csv.py
+++ 21_custom_module/library/facts_to_csv.py
@@ -9,25 +9,27 @@ def main():
             dest_csv_file = dict(type='str', required=False, default='facts_file.csv')
         )
     )
-    
-    device_facts= module.params['device_facts']
-    sorted_device_facts = dict(sorted(device_facts.items()))
-    csv_filename= f"{module.params['dest_csv_path']}/{module.params['dest_csv_file']}"
-    csv_header = ["inventory_name", "hostname", "model", "version", "serial_number"]
+    try:
+        device_facts= module.params['device_facts']
+        sorted_device_facts = dict(sorted(device_facts.items()))
+        csv_filename= f"{module.params['dest_csv_path']}/{module.params['dest_csv_file']}"
+        csv_header = ["inventory_name", "hostname", "model", "version", "serial_number"]
 
-    with open(csv_filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_header)
-        writer.writeheader()
-        for device, details in sorted_device_facts.items():
-            writer.writerow({
-                "inventory_name": device, 
-                "hostname": details['net_hostname'], 
-                "model": details['net_model'],
-                "version": details['net_version'], 
-                "serial_number": details['net_serialnum']
-                })
-            
-    module.exit_json(changed=True, output="Saved content to csv")
-
+        with open(csv_filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_header)
+            writer.writeheader()
+            for device, details in sorted_device_facts.items():
+                writer.writerow({
+                    "inventory_name": device, 
+                    "hostname": details['net_hostname'], 
+                    "model": details['net_model'],
+                    "version": details['net_version'], 
+                    "serial_number": details['net_serialnum']
+                    })
+                
+        module.exit_json(changed=True, output="Saved content to csv")
+    except Exception as e:
+        module.fail_json(msg='Module failed', exception=str(e))
+        
 if __name__ == '__main__':
     main()
